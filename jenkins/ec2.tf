b03339a5223ed535ec417f48631970122e64ec71
Ezheg <tgrib@yahoo.com>
2021-11-21 23:27:33 +0000

    change to amazon ami

--- jenkins/ec2.tf
+++ jenkins/ec2.tf
@@ -1,6 +1,6 @@
 locals {
   common_tags = {
-    Name = "HelloWorld"
+    Name = "Jenkins"
     Env  = "Dev"
     Team = "DevOps"
   }
@@ -47,7 +47,7 @@ resource "aws_security_group" "allow_tls" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-ae6272b8"
+  ami                    = "ami-04902260ca3d33422"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   key_name               = aws_key_pair.edv.key_name
@@ -56,32 +56,6 @@ resource "aws_instance" "web" {
   tags = local.common_tags
 
 
-  # resource "null_resource" "web" {
-  # # Changes to any instance of the cluster requires re-provisioning
-  # triggers = {
-  #   aws_instance_ids = web(",", aws_instance.web.*.id)
-  # }
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("~/.ssh/id_rsa")
-    host        = "aws_instance.web.public_ip"
-  }
-
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum install wget -y",
-      "sudo wget -O /etc/yum.repos.d/jenkins.repo  https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
-      "sudo yum upgrade",
-      "sudo yum install epel-release java-11-openjdk-devel",
-      "sudo yum install jenkins",
-      "sudo systemctl daemon-reload",
-      "sudo systemctl start jenkins",
-    ]
-  }
 }
 
 resource "aws_route53_record" "www" {
@@ -90,4 +64,56 @@ resource "aws_route53_record" "www" {
   type    = "A"
   ttl     = "300"
   records = [aws_instance.web.public_ip]
+}
+# resource "null_resource" "web" {
+#   # Changes to any instance of the cluster requires re-provisioning
+#   triggers =   {
+#     aws_instance_ids =  "aws_instance.web.id"
+#   }
+#   }
+resource "null_resource" "example" {
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      host = aws_instance.web.public_dns
+      user = "ec2-user"
+      agent = "false"
+      private_key = file("~/.ssh/id_rsa")
+    }
+
+    inline = ["echo 'connected!'",
+               "sudo yum install wget -y",
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo  https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
+      "sudo yum upgrade",
+      "sudo yum install epel-release java-11-openjdk-devel",
+      "sudo yum install jenkins",
+      "sudo systemctl daemon-reload",
+      "sudo systemctl start jenkins",]   
+    
+  }
+  # connection {
+  #   type        = "ssh"
+  #   user        = "ec2-user"
+  #   private_key = file("~/.ssh/id_rsa")
+  #   port = "22"
+  #   host        = "aws_instance.web.public_ip"
+  #   agent = "true"
+  #   timeout = "10m"
+        
+  # }
+  
+
+
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "sudo yum install wget -y",
+  #     "sudo wget -O /etc/yum.repos.d/jenkins.repo  https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+  #     "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
+  #     "sudo yum upgrade",
+  #     "sudo yum install epel-release java-11-openjdk-devel",
+  #     "sudo yum install jenkins",
+  #     "sudo systemctl daemon-reload",
+  #     "sudo systemctl start jenkins",
+  #   ]
 }
