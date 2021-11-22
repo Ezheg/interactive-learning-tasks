248f4579c5c0ab32879fd22f674f9dcdf8a1088f
Ezheg <tgrib@yahoo.com>
2021-11-22 23:53:54 +0000

    add 60g volume

--- nagiosxi/ec2.tf
+++ nagiosxi/ec2.tf
@@ -1,6 +1,6 @@
 locals {
   common_tags = {
-    Name = "Jenkins"
+    Name = "Nagios"
     Env  = "Dev"
     Team = "DevOps"
   }
@@ -47,52 +47,60 @@ resource "aws_security_group" "allow_tls" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-04902260ca3d33422"
+  ami                    = "ami-0279c3b3186e54acd"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   key_name               = aws_key_pair.edv.key_name
   availability_zone      = "us-east-1a"
   # user_data              = file("userdata.sh")
   tags = local.common_tags
-
-
+}
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-east-1a"
+  size              = 60
+  tags              = local.common_tags
 }
 
-resource "aws_route53_record" "www" {
-  zone_id = "Z0195983MG9WNG6OJ115"
-  name    = "jenkins.timgrib.com"
-  type    = "A"
-  ttl     = "300"
-  records = [aws_instance.web.public_ip]
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.web.id
 }
-# resource "null_resource" "web" {
-#   # Changes to any instance of the cluster requires re-provisioning
-#   triggers =   {
-#     aws_instance_ids =  "aws_instance.web.id"
-#   }
-#   }
+
+
+
+# resource "aws_route53_record" "www" {
+#   zone_id = "Z0195983MG9WNG6OJ115"
+#   name    = "jenkins.timgrib.com"
+#   type    = "A"
+#   ttl     = "300"
+#   records = [aws_instance.web.public_ip]
+# }
+
 resource "null_resource" "example" {
   provisioner "remote-exec" {
     connection {
       type        = "ssh"
       host        = aws_instance.web.public_dns
-      user        = "ec2-user"
+      user        = "ubuntu"
       agent       = "false"
       private_key = file("~/.ssh/id_rsa")
     }
 
     inline = ["echo 'connected!'",
-      "sudo yum install wget -y",
-      "sudo wget -O /etc/yum.repos.d/jenkins.repo  https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
-       "sudo amazon-linux-extras install epel -y",
-       "sudo yum upgrade -y",
-      "sudo yum install jenkins java-1.8.0-openjdk-devel -y",
-      "sudo yum install jenkins -y",
-      "sudo systemctl daemon-reload",
-      "sudo systemctl start jenkins", ]
-
-  }
+      #  "sudo useradd ansible",
+      "cd /tmp",
+      # "sudo -E add-apt-repository ppa:ansible/ansible",
+      "sudo apt-get update",
+      "wget https://assets.nagios.com/downloads/nagiosxi/xi-latest.tar.gz",
+       "tar xzf xi-latest.tar.gz",
+       "cd nagiosxi",
+       "sudo chmod +x fullinstall",
+      "yes | sudo bash fullinstall "]
+      # "sudo yum install jenkins -y",
+      # "sudo systemctl daemon-reload",
+      # "sudo systemctl start jenkins", ]
+  
   # connection {
   #   type        = "ssh"
   #   user        = "ec2-user"
@@ -117,4 +125,5 @@ resource "null_resource" "example" {
   #     "sudo systemctl daemon-reload",
   #     "sudo systemctl start jenkins",
   #   ]
+}
 }
--- nagiosxi/output.tf
+++ nagiosxi/output.tf
@@ -1,11 +1,11 @@
-output "name" {
-  value = aws_route53_record.www.name
+output "ip" {
+  value = aws_instance.web.public_ip
 }
-output "namelogin" {
-  value = "administrator"
-  # value = "c4df25c881c34319b1aa246e85ca00c8"
-}
-output "pass" {
-  # value = "administrator"
-  value = "c4df25c881c34319b1aa246e85ca00c8"
-}
+# output "namelogin" {
+#   value = "administrator"
+#   # value = "c4df25c881c34319b1aa246e85ca00c8"
+# }
+# output "pass" {
+#   # value = "administrator"
+#   value = "c4df25c881c34319b1aa246e85ca00c8"
+# }
